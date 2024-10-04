336bae67040204bfa39d42ac470eb212c87d394b
Ashish Saxena <ashish.sxn847@gmail.com>
2024-10-04 20:12:26 +0530

    Creating Next App

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+
+export default function HeroSection() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0" >
+        <div className="p-4 relative z-10 w-full text-center">
+            <h1>Master the art of music</h1>
+            <p>
+                Dive Into our comprehensive music corses and transform your musical journey today.
+                Whether 
+            </p>
+        </div>
+    </div>
+  )
+}
