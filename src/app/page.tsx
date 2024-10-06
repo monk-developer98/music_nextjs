0cb1c313e7299e25dfdd7ebd3a706bd629fc3d09
Ashish Saxena <ashish.sxn847@gmail.com>
2024-10-06 12:11:54 +0530

    Webinar Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 // import Image from "next/image";
 
+import FeaturedCorses from "@/components/FeaturedCorses";
 import HeroSection from "@/components/HeroSection";
+import TestimoniolCards from "@/components/TestimoniolCards";
+import UpComingWebinars from "@/components/UpComingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCorses />
+      <WhyChooseUs />
+      <TestimoniolCards/>
+      <UpComingWebinars />
     </main>
   );
 }
